3aec56b8b47a8a5ece6cb49bc023b72f9b1bc9c3
majaarvola <majaarvola@gmail.com>
2018-12-07 02:47:26 +0900

    Finished project

--- autoencoder.py
+++ autoencoder.py
@@ -1,15 +1,66 @@
+# import modules
 from keras.layers import Input, Dense
 from keras.models import Model
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-# this is the size of our encoded representations
-encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+# load the preprocessed data
+COG1 = np.load("preprocessed_data/COG1_prob.npy")
+COG160 =  np.load("preprocessed_data/COG160_prob.npy")
+COG161 =  np.load("preprocessed_data/COG161_prob.npy")
+all_families = np.concatenate((COG1,COG160,COG161))
 
-# this is our input placeholder
-input_img = Input(shape=(784,))
+# divide data into train (75%) and test (25%)
+x_train, x_test = train_test_split(all_families, test_size=0.25)
+
+# state the dimensions to use in autoencoder
+input_dim = 400 # our sequences has dimension 400x1
+encoding_dim = 25  # We want to compress the dimension with a factor of 16, from 400 to 25
+
+# placeholder for input and encoded input
+input = Input(shape=(input_dim,)) 
+encoded_input = Input(shape=(encoding_dim,))
+
 # "encoded" is the encoded representation of the input
-encoded = Dense(encoding_dim, activation='relu')(input_img)
+encoded = Dense(encoding_dim, activation='relu')(input)
 # "decoded" is the lossy reconstruction of the input
-decoded = Dense(784, activation='sigmoid')(encoded)
+decoded = Dense(400, activation='sigmoid')(encoded)
 
-# this model maps an input to its reconstruction
-autoencoder = Model(input_img, decoded)
+# Create the autoencoder model and for the encoder part
+autoencoder = Model(input, decoded)
+encoder = Model(input, encoded)
+
+# Get the last layer of decoder part and create the decoder model
+decoder_layer = autoencoder.layers[-1]
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+# compile the autoencoder with some optimiser and loss function
+autoencoder.compile(optimizer='adam', loss='mean_squared_logarithmic_error')
+
+# train the model 
+result = autoencoder.fit(x_train, x_train,
+                epochs=50,
+                batch_size=10,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+# extract encoded vector for sequences
+encoded_vector = encoder.predict(all_families)  
+
+# print encoded vector and save to txt file
+print("The resulting encoded vector is: ")      
+print(encoded_vector)
+np.savetxt('resultat.txt', encoded_vector, delimiter=',') 
+
+# visualisation of change in loss during training to make sure in converges
+plt.figure
+plt.plot(result.history['loss'], color ='green')
+plt.plot(result.history['val_loss'], color ='orchid')
+plt.xlim(0,50-1)
+plt.title('Model Loss')
+plt.ylabel('Loss Value')
+plt.xlabel('Number of Epochs')
+plt.legend(['Loss for training data', 'Loss for test data']) #
+plt.show()
--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,4 @@
+# Import modules
 from Bio import SeqIO, Seq, Alphabet
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC
@@ -5,14 +6,14 @@ from itertools import product
 import numpy as np
 import re
 
-k_size = 2
+# Status:
+print("Starting preprocessing...")
 
 # Import protein letters and compute possible permutations of k-mers
+k_size = 2
 chars = IUPAC.IUPACProtein.letters
 kmer = [''.join(c) for c in product(chars, repeat=k_size)]
 n = len(kmer)
-#print('Nr. of possible k-mers:', n)
-#print(kmer)
 
 # Method for reading fasta file and return sequence
 def read_sequence(filename):
@@ -23,9 +24,9 @@ def read_sequence(filename):
         return sequences
 
 # Read the sequences from 3 COG families:
-COG1 = read_sequence("data/COG1.fasta")
-COG160 = read_sequence("data/COG160.fasta")
-COG161 = read_sequence("data/COG161.fasta")
+COG1 = read_sequence("fasta_files/COG1.fasta")
+COG160 = read_sequence("fasta_files/COG160.fasta")
+COG161 = read_sequence("fasta_files/COG161.fasta")
 
 
 # Counting k-mers in a sequence
@@ -37,22 +38,24 @@ def count_occurence(seq):
             count[i] = temp
         return count
 
-#for seq in family:        
-
-def kmer_prob(family):
+#Start a counting for each sequence in a family:        
+def kmer_count(family):
     kmer_matrix = np.zeros((len(family),n))
     index = 0
     for seq in family:
-        kmer_matrix[index,:] = count_occurence(seq)
+        kmer_matrix[index,:] = count_occurence(seq)/(len(seq)+2-k_size)
         index = index+1
-    return kmer_matrix/len(family)
+    return kmer_matrix
 
 #Count how many times the k-mers appears in all families
-COG1_prob = kmer_prob(COG1)
-COG160_prob = kmer_prob(COG160)
-COG161_prob = kmer_prob(COG161)
+COG1_prob = kmer_count(COG1)
+COG160_prob = kmer_count(COG160)
+COG161_prob = kmer_count(COG161)
 
 # Store the result in a npy file
-np.save("data/COG1_prob.npy", COG1_prob)
-np.save("data/COG160_prob.npy", COG160_prob)
-np.save("data/COG161_prob.npy", COG161_prob)
+np.save("preprocessed_data/COG1_prob.npy", COG1_prob)
+np.save("preprocessed_data/COG160_prob.npy", COG160_prob)
+np.save("preprocessed_data/COG161_prob.npy", COG161_prob)
+
+# Status:
+print("Preprocessing done!")
